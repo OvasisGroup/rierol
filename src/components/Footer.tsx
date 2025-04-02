15e911ebec4b56e667fdab55ae1c1ea9e20db1a8
sociocreatives <ayaraerick@gmail.com>
2025-04-02 16:06:03 +0300

    first commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,8 @@ export default function Footer() {
   return (
     <div className='bg-primary flex flex-col justify-center items-center py-10'>
         <Image src="/images/rierol-logo.png" alt="logo" width={120} height={100} className='width={100} py-4' />
-        <h3 className='text-white max-w-lg text-center'>Operating since 2011 and registered in Kenya as a limited entity in 2014, Rierol Enterprises Limited (REL) is a renowned weighing scale and ICT solutions provider.</h3>
-        <h1 className='text-white pt-5 font-bold' >Copyright,2025 - All rights reserved, Rierol Enterprises Limited</h1>
+        <h3 className='text-white md:max-w-lg px-6 text-center'>Operating since 2011 and registered in Kenya as a limited entity in 2014, Rierol Enterprises Limited (REL) is a renowned weighing scale and ICT solutions provider.</h3>
+        <h1 className='text-white pt-5 font-bold text-center' >Copyright,2025 - All rights reserved, Rierol Enterprises Limited</h1>
     </div>
   )
 }
